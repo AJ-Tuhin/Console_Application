12322446ce8ea44b01c628ed32347b4ddaef8462
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-26 00:59:16 +0600

    Add files via upload

---
+++ Basic/13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}{0}{0}",number);
+            Console.WriteLine("{0} {0}", number);
+            Console.WriteLine("{0} {0}", number);
+            Console.WriteLine("{0} {0}", number);
+            Console.WriteLine("{0}{0}{0}", number);
+
+            Console.ReadKey();
+        }
+    }
+}
