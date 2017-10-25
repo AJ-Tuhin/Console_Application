de5c2f1ae498cace31ca7297156a935b657c4980
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-26 03:26:36 +0600

    Add files via upload

---
+++ Basic/23/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23._.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "write a c# sharp program to display the following pattern using the alphabet.";
+
+            Console.WriteLine(str.ToLower());
+
+            Console.ReadKey();
+        }
+    }
+}
