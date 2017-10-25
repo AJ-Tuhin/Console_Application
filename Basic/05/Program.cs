ade4cc49db6f474f7472d467eab85c4470dbc61b
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-26 00:51:47 +0600

    Add files via upload

---
+++ Basic/05/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            int temp = firstNumber;
+            firstNumber = secondNumber;
+            secondNumber = temp;
+
+            Console.WriteLine("After Swapping : ");
+            Console.WriteLine("First Number : " + firstNumber);
+            Console.WriteLine("Second Number : " + secondNumber);
+            
+
+            Console.ReadKey();
+        }
+    }
+}
