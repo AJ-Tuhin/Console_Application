0da96737c4c6a7403af7d0b234de3bbf66ba98b5
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-26 03:22:34 +0600

    Add files via upload

---
+++ Basic/20/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(DifferenceNumber(firstNumber,secondNumber));
+
+            Console.ReadKey();
+
+        }
+
+        public static int DifferenceNumber(int a,int b)
+        {
+            return (a > b)
+                ? Math.Abs(a - b)*2
+                : b-a;
+
+        }
+    }
+}
