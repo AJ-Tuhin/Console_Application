d944c4deca9d9fbbba44f1ee63856b82415636b9
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-26 00:52:22 +0600

    Add files via upload

---
+++ Basic/06/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the first number to multiply: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Input the second number to multiply: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Input the third number to multiply: ");
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int result = firstNumber*secondNumber*thirdNumber;
+
+            Console.WriteLine("Expected Output:");
+            Console.WriteLine("{0} x {1} x {2} = {3}",firstNumber,secondNumber,thirdNumber,result);
+
+            Console.Read();
+        }
+    }
+}
