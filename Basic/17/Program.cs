ad9da657f6abbbbc8ca4fab55782e06aa05f56e9
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-26 01:04:27 +0600

    Add files via upload

---
+++ Basic/17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string str = Console.ReadLine();
+
+            string s = str.Substring(0, 1);
+
+            string str2 = s + str + s;
+            Console.WriteLine(str2);
+
+            Console.ReadKey();
+        }
+    }
+}
