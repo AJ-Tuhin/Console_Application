6c26680878dcdddc8d21c27c5dd28fe9c8475307
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-26 03:25:23 +0600

    Add files via upload

---
+++ Basic/22/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input an integer:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(result(x));
+            Console.ReadKey();
+        }
+
+        public static bool result(int n)
+        {
+            if (Math.Abs(n - 100) <= 10 || Math.Abs(n - 200) <= 10)
+                return true;
+            return false;
+        }
+    }
+}
