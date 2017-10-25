377faaf7565a17c5ebc08a33de217a910c2a95a0
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-26 00:53:21 +0600

    Add files via upload

---
+++ Basic/07/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the first number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Input the second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int add = firstNumber + secondNumber;
+            int sub = firstNumber - secondNumber;
+            int multiply = firstNumber * secondNumber;
+            int divide = firstNumber / secondNumber;
+            int mod = firstNumber%secondNumber;
+
+            Console.WriteLine("Expected Output: ");
+            Console.WriteLine("{0} + {1} = {2}",firstNumber,secondNumber,add);
+            Console.WriteLine("{0} - {1} = {2}", firstNumber, secondNumber, sub);
+            Console.WriteLine("{0} * {1} = {2}", firstNumber, secondNumber, multiply);
+            Console.WriteLine("{0} / {1} = {2}", firstNumber, secondNumber, divide);
+            Console.WriteLine("{0} mod {1} = {2}", firstNumber, secondNumber, mod);
+
+            Console.Read();
+        }
+    }
+}
