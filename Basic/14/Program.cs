13fd4d67521ef4d7998d3d8fa406fb57a0b6ff42
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-26 01:00:31 +0600

    Add files via upload

---
+++ Basic/14/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the amount of celsius: ");
+            int celsius = int.Parse(Console.ReadLine());
+
+            int kelvin = celsius + (int)273.15;
+            double fahrenheit = (celsius *1.8 ) + 32;
+
+            Console.WriteLine("Kelvin = {0}",kelvin);
+            Console.WriteLine("Fahrenheit = {0}",fahrenheit);
+
+            Console.ReadKey();
+        }
+    }
+}
