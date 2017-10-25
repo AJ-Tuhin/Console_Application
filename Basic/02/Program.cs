a33ab218704b6b1963e9b7e88994322f87783d05
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-26 00:34:48 +0600

    Add files via upload

---
+++ Basic/02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(15+17);
+
+            Console.ReadKey();
+
+        }
+    }
+}
