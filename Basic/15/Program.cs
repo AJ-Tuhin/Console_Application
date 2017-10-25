9eb4e23ebf366e49ad7499859b19483b2219ec01
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-26 01:02:10 +0600

    Add files via upload

---
+++ Basic/15/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.RemoveCharApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Remove_Char("w3resource",1));
+            Console.WriteLine(Remove_Char("w3resource",9));
+            Console.WriteLine(Remove_Char("w3resource",0));
+
+            Console.ReadKey();
+
+        }
+
+        public static string Remove_Char(string str,int n)
+        {
+            return str.Remove(n, 1);
+        }
+    }
+}
