430311c960d27be430e26f1dd2a04f62ddd7df0f
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-26 00:55:10 +0600

    Add files via upload

---
+++ Basic/09/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Data:");
+            Console.Write("Enter the First number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the third number: ");
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the fourth number: ");
+            int fourthNumber = int.Parse(Console.ReadLine());
+
+            int add = firstNumber + secondNumber + thirdNumber + fourthNumber;
+            int average = add/4;
+
+            Console.WriteLine("Expected Output:");
+            Console.WriteLine("The average of {0} , {1} , {2} , {3} is: {4} ",firstNumber,secondNumber,thirdNumber,fourthNumber, average);
+            Console.Read();
+
+        }
+    }
+}
