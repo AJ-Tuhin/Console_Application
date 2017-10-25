23416797df76a81280c84fe296106dd7f18b62d5
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-26 03:21:29 +0600

    Add files via upload

---
+++ Basic/19/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(SumTriple(firstNumber,secondNumber));
+
+            Console.ReadKey();
+        }
+
+        public static int SumTriple(int a,int b)
+        {
+            return (a == b)
+                ? (a+b)*3 : a+b;
+        }
+    }
+}
