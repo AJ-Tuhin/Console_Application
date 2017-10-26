50812ffcb3f27c3d02b4e15b985e500f1c5385c0
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-27 03:18:44 +0600

    Add files via upload

---
+++ Basic/36/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _36.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a first number: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input a second number: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine((firstNumber>= -10 && firstNumber<=10)||(secondNumber>=-10 && secondNumber<=10));
+            Console.ReadKey();
+        }
+    }
+}
