3a055388eb3fc7951b39a76934c6da9614e64a29
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-26 19:31:44 +0600

    Add files via upload

---
+++ Basic/28/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string astring = "Display the pattern like pyramid using the alphabet";
+            char[] cArray = astring.ToCharArray();
+            string reverse = String.Empty;
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                reverse += cArray[i];
+            }
+            Console.WriteLine(reverse);
+            Console.ReadKey();
+        }
+    }
+}
