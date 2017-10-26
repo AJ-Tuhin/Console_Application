51e19dd35e2c6a8511c0a405c7085f823a9f04b1
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-27 03:19:35 +0600

    Add files via upload

---
+++ Basic/37/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Test Data: ");
+            string data = Console.ReadLine();
+            
+
+            if (data.Substring(1, 2).Equals("hp"))
+            {
+                string sb = data.Remove(1, 2);
+                Console.WriteLine(sb);
+            }
+            else
+            {
+                Console.WriteLine("There are no 'hp' in second index");
+            }
+
+            
+            Console.ReadKey();
+        }
+    }
+}
