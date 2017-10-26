3fd7f2b7ba60f0459739765eee2b90beb2860982
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-26 19:32:54 +0600

    Add files via upload

---
+++ Basic/29/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo f = new FileInfo(@"E:\Exercise\self practise\Basic C#\000.problems\01. Problem.txt");
+
+            Console.WriteLine("Size of a file:{0}",f.Length.ToString());
+            Console.ReadKey();
+        }
+    }
+}
