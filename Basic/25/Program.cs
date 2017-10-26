acf81193e9785a8f2cc7db9845f647db430aeed8
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-26 19:28:45 +0600

    Add files via upload

---
+++ Basic/25/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Odd numbers from 1 to 99. Prints one number per line.");
+            for (int i = 1; i <= 99; i++)
+            {
+                if (i%2 !=0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
