870887afe40f4875ab1a12fad8b3c1e92f76a503
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-27 03:14:59 +0600

    Add files via upload

---
+++ Basic/32/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _32.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string astring = Console.ReadLine();
+
+
+           Console.WriteLine(Format(astring));
+           Console.ReadKey();
+        }
+
+        private static string Format(string astring)
+        {
+            return astring.Length < 4 ? astring + astring + astring + astring:astring.Substring(astring.Length-4)+ astring.Substring(astring.Length - 4)+ astring.Substring(astring.Length - 4)+ astring.Substring(astring.Length - 4);
+        }
+    }
+}
