d2fe69a2d63d34501ee2bc2f97e55f2d124893fb
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-27 03:21:25 +0600

    Add files via upload

---
+++ Basic/40/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _40.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first Integer: ");
+            int fNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second Integer: ");
+            int sNumber = Convert.ToInt32(Console.ReadLine());
+
+            int a = Math.Abs(20 - fNumber);
+            int b = Math.Abs(20 - sNumber);
+            if (fNumber==sNumber)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                Console.WriteLine(a > b ? sNumber : fNumber);
+            }
+            Console.ReadKey();
+        }
+    }
+}
