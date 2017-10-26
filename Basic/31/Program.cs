f9b356aab83c4cdb9c468d61fe3ba6edf4cc0ef8
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-26 19:35:54 +0600

    Add files via upload

---
+++ Basic/31/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 4;
+            int[] array1 = new int[4] { 1, 3, -5, 4 };
+            int[] array2 = new int[4] { 1, 4, -5, -2 };
+            int[] array3 = new int[4];
+            
+            Console.WriteLine("Array1: [{0}]",string.Join(" ",array1));
+            Console.WriteLine("Array2: [{0}]",string.Join(" ",array2));
+
+            for (int i = 0; i <array1.Length; i++)
+            {
+                array3[i] = array1[i]*array2[i];
+
+            }
+            //for (int i = 0; i < array3.Length; i++)
+            //{
+            //    Console.Write(array3[i] + " ");
+            //}
+
+            foreach (int value in array3)
+            {
+                Console.Write(value+" ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
