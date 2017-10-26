53b19ed83350c745dbd7b39442d9e2d9bdb2026c
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-27 03:15:52 +0600

    Add files via upload

---
+++ Basic/33/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _33.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input first integer: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(number%3==0 || number%7==0);
+            Console.ReadKey();
+        }
+    }
+}
