a263e2570eed1138b7702bd34d4c6090b7f631a7
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-26 19:29:42 +0600

    Add files via upload

---
+++ Basic/26/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count;
+            int i;
+            int j;
+            int count2 = 0;
+            int result = 0;
+            Console.WriteLine("Sum of the first 500 prime numbers: ");
+            
+                for (i = 2;count2 < 500; i++)
+                {
+                    count = 0;
+
+                    for (j = 2; j <= i / 2; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            count++;
+                            break;
+                        }
+                    }
+
+
+                    if (count == 0 && i != 1)
+                    {
+                        Console.WriteLine(i);
+                        result += i;
+                        count2++;
+                    }
+
+
+                }
+            Console.WriteLine("sum is {0}", result);
+            Console.ReadKey();
+        }
+    }
+}
