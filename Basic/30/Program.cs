eccb09468621bc11dd881427f0ae715ecccc647c
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-26 19:34:52 +0600

    Add files via upload

---
+++ Basic/30/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hexadecimal number: ");
+            string hexadesiNumber = Console.ReadLine();
+
+            int deciValue = Convert.ToInt32(hexadesiNumber, 16);
+
+            Console.WriteLine(deciValue);
+            Console.ReadKey();
+        }
+    }
+}
