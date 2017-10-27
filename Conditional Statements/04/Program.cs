87ca2e0d6e68a3127e5850249cc766137b3969bb
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-28 00:14:43 +0600

    Add files via upload

---
+++ Conditional Statements/04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(year%4==0?@"{0} is a leap year.": "{0} is not a leap year.",year);
+            Console.ReadKey();
+        }
+    }
+}
