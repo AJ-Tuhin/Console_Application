e55161849d2cd7f76be414f9cc7ed55fdcc9bd8b
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-27 20:21:22 +0600

    Add files via upload

---
+++ Data Types/06/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("x = y2 + 2y + 1 ");
+
+            for (int y = -5; y <=5; y++)
+            {
+                int x = y*y - 2*(y) + 1;
+                Console.WriteLine("y={0}, x= ({0})2 + 2*{0} + 1={1}",y,x);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
