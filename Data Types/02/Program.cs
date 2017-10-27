019da1e23de44f0233438ef6d90ee00f96e8faa7
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-27 20:17:00 +0600

    Add files via upload

---
+++ Data Types/02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Enter a number: 6
+            //Enter the desired width: 6
+
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the desired width: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            int reduceWidth = width;
+
+            for (int i = 1; i <= width; i++)
+            {
+                for (int j = reduceWidth; j >0; j--)
+                {
+                    Console.Write(number);
+                }
+                reduceWidth -= 1;
+                Console.WriteLine();
+            }
+
+            Console.Read();
+        }
+    }
+}
