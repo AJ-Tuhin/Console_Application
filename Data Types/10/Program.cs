3b9e3be2b8b20af1c2f5f34fc245e0e701892a43
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-27 20:24:22 +0600

    Add files via upload

---
+++ Data Types/10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int fNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int sNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(fNumber%2==0 && sNumber%2==0?true:false);
+            Console.ReadKey();
+        }
+    }
+}
