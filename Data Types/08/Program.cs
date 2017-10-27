c6badf9aafad52dd3085243e957c19d7f329189f
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-27 20:22:59 +0600

    Add files via upload

---
+++ Data Types/08/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Radius: ");
+            double radious = Convert.ToDouble(Console.ReadLine());
+
+            const double pi = 3.1416;
+            double surface = 4*pi*(radious*radious);
+            double volume = (4f/3f) * pi*(radious*radious*radious);
+
+            Console.WriteLine("surface= {0}",surface);
+            Console.WriteLine("volume= {0}",volume);
+
+            Console.ReadKey();
+        }
+    }
+}
