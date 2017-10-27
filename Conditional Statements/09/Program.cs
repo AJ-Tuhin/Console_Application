5eb06bfd56b162e8845d1026f7a71d0fca84c380
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-28 00:21:11 +0600

    Add files via upload

---
+++ Conditional Statements/09/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the value for X coordinate: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input the value for Y coordinate: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            if (x>0 && y>0)
+            {
+                Console.WriteLine("The coordinate point({0}, {1}) lies in the First quadrant.",x,y);
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("The coordinate point({0}, {1}) lies in the Second quadrant.", x, y);
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("The coordinate point({0}, {1}) lies in the Third quadrant.", x, y);
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.WriteLine("The coordinate point({0}, {1}) lies in the Fourth quadrant.", x, y);
+            }
+            else
+            {
+                Console.WriteLine("The coordinate point ({0} {1}) lies at the origin.",x,y);
+            }
+            Console.ReadKey();
+        }
+    }
+}
