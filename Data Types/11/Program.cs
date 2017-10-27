6f2bd06490ef6cfeab45756b1b8f6c20c5b6c2fa
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-27 20:25:08 +0600

    Add files via upload

---
+++ Data Types/11/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int reminder, result;
+            string[]astring = new string[100];
+            for (int i = 0; number>=1; i++)
+            {
+                reminder = number%2;
+                astring[i] = reminder.ToString();
+                result = number/2;
+                number = result;
+            }
+            
+            Array.Reverse(astring);
+            foreach (var value in astring)
+            {
+                Console.Write(value);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
