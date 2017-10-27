2842b40cdc6967e154694e4898ce367e47f98a37
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-27 20:18:19 +0600

    Add files via upload

---
+++ Data Types/03/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userId1 = "tuhincse001@gmail.com";
+            string password1 = "131152540";
+
+            Console.Write("Enter user id: ");
+            string userId = Console.ReadLine();
+
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            for (int i = 0; i < 2 && (userId != userId1 || password != password1); i++)
+            {
+                Console.WriteLine("Enter id and password again ");
+
+                Console.Write("Enter user id: ");
+                userId = Console.ReadLine();
+
+                Console.Write("Enter password: ");
+                password = Console.ReadLine();
+
+            }
+            if (userId == userId1 || password == password1)
+            {
+                Console.WriteLine("you are successfully log in");
+            }
+            else
+            {
+                Console.WriteLine("you are rejected");
+            }
+            Console.Read();
+
+        }
+    }
+}
