570b54fb92ea9ac45a48e233f6cede066b3e5671
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-28 00:16:19 +0600

    Add files via upload

---
+++ Conditional Statements/06/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the value of m: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            int n;
+            if (m>0)
+            {
+                n = 1;
+            }
+            else if (m<0)
+            {
+                n = -1;
+            }
+            else
+            {
+                n = 0;
+            }
+            Console.WriteLine("The value of m = {0}",m);
+            Console.WriteLine("The value of n = {0}", n);
+            Console.ReadKey();
+        }
+    }
+}
