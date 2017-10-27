0ff6ece675ae987b7b091f788a2b5f596783d088
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-28 00:13:14 +0600

    Add files via upload

---
+++ Conditional Statements/02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number%2==0)
+            {
+                Console.WriteLine("{0} is an even integer",number);
+            }
+            else
+            {
+                Console.WriteLine("{0} is an odd integer", number);
+            }
+            Console.ReadKey();
+        }
+    }
+}
