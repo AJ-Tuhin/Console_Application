2eead315eddf9749fca412754ecf31df4d424cdb
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-28 00:17:06 +0600

    Add files via upload

---
+++ Conditional Statements/07/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your height(in centimeter): ");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            if (height<150)
+            {
+                Console.WriteLine("you are Dwarf.");
+            }
+            else if (height >150 && height<165)
+            {
+                Console.WriteLine("you are average.");
+            }
+            else if (height > 165 && height < 195)
+            {
+                Console.WriteLine("you are tall.");
+            }
+            else
+            {
+                Console.WriteLine("your height is abnormal.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
