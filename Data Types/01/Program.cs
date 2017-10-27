92f0567bc40286850cf04493a13974f6090daae6
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-27 20:15:58 +0600

    Add files via upload

---
+++ Data Types/01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter latter: ");
+            char latter1 = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Enter latter: ");
+            char latter2 = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Enter latter: ");
+            char latter3 = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("{0} {1} {2}",latter3,latter2,latter1);
+
+            Console.Read();
+        }
+    }
+}
