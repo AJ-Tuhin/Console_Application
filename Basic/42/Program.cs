7811988442c270713d91118c79bea4103ba95d62
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-27 12:02:57 +0600

    Add files via upload

---
+++ Basic/42/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _42.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string str = Console.ReadLine();
+
+            if (str.Length < 4)
+            {
+                Console.WriteLine(str.ToUpper());
+            }
+            else
+            {
+                Console.WriteLine(str.Substring(0, 4).ToLower() + str.Substring(4, str.Length - 4));
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
