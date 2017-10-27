dfe2021d4a3e7b7984c5225c57e932834d8cd4ef
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-28 00:22:07 +0600

    Add files via upload

---
+++ Conditional Statements/10/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the marks obtained in Math: ");
+            int math = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input the marks obtained in Physics: ");
+            int phy = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input the marks obtained in Chemestry: ");
+            int che = Convert.ToInt32(Console.ReadLine());
+
+            int total = math + phy + che;
+            int totalMathAndPhy = math + phy;
+            
+            if (math >= 65)
+            {
+                if (phy >= 55)
+                {
+                    if (che >= 50)
+                    {
+                        if ((total >= 180) || (totalMathAndPhy >= 140))
+                        {
+                            Console.WriteLine("The candidate is eligible for admission.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("The candidate is not eligible for admission.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("The candidate is not eligible for admission.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The candidate is not eligible for admission.");
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("The candidate is not eligible for admission.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
