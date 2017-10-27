c20944f8f6832505ee61a27f51793e330d69e62f
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-27 20:20:27 +0600

    Add files via upload

---
+++ Data Types/05/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.1416;
+            Console.Write("Input the radius of the circle: ");
+            int radius = Convert.ToInt32(Console.ReadLine());
+            double area = pi*(radius*radius);
+            double paramiter = 2*pi*radius;
+
+            Console.WriteLine("Perimeter of Circle :{0}",paramiter);
+            Console.WriteLine("Area of the circle:{0}",area);
+            Console.ReadKey();
+        }
+    }
+}
