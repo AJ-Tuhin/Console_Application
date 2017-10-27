3b65c97e2bb2d787a0b48be44888134114feee6e
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-28 00:15:31 +0600

    Add files via upload

---
+++ Conditional Statements/05/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age>18)
+            {
+                Console.WriteLine("Congratulation! You are eligible for casting your vote.");
+            }
+            else
+            {
+                Console.WriteLine("You are not eligible for casting your vote.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
