9434924c42431d8fc6c7a962099e7f98f6b4c7f3
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-27 20:23:42 +0600

    Add files via upload

---
+++ Data Types/09/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a symbol: ");
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            if (ch=='a'|| ch == 'e'|| ch == 'i'|| ch == 'o'|| ch == 'u')
+            {
+                Console.WriteLine("it is a vowel");
+            }
+            else if (ch>'1' && ch<='9')
+            {
+                Console.WriteLine("It is a number");
+            }
+            else
+            {
+                Console.WriteLine("it is other symbol");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
