209c220204d05ad0c17e977413b083b9552d9048
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-28 00:12:25 +0600

    Add files via upload

---
+++ Conditional Statements/01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input 1st number: ");
+            int fNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input 2nd number: ");
+            int sNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (fNumber==sNumber)
+            {
+                Console.WriteLine("{0} and {1} are equal",fNumber,sNumber);
+            }
+            else
+            {
+                Console.WriteLine("{0} and {1} are not equal", fNumber, sNumber);
+            }
+            Console.ReadKey();
+        }
+    }
+}
