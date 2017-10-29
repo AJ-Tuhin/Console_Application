97718cf36de2db10f5364b6554c70893c0a4a7e4
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-29 10:38:41 +0600

    Add files via upload

---
+++ Conditional Statements/16/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an alphabet: ");
+            char ch = Convert.ToChar(Console.ReadLine());
+            if ((ch>='a' && ch<='z') || (ch >= 'A' && ch <= 'Z'))
+            {
+                if ((ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') || 
+                    (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'))
+                {
+                    Console.WriteLine("The alphabet is a vowel.");
+                }
+                else
+                {
+                    Console.WriteLine("The alphabet is a consonant.");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("your entered item is not alphabet");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
