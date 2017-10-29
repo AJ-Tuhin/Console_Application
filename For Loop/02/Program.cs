ebf9b4517e29967b4a4bc347f22c90ae4b7b9baa
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-29 12:47:50 +0600

    Add files via upload

---
+++ For Loop/02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            Console.WriteLine("The first 10 natural number is:");
+            for (int i = 1; i < 11; i++)
+            {
+                Console.Write(i+" ");
+                result += i;
+            }
+            Console.WriteLine();
+            Console.WriteLine("The sum is {0}",result);
+            Console.ReadKey();
+        }
+    }
+}
