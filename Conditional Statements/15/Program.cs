3e62e396477346e5d562bb531dcea3af79e25124
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-29 10:32:26 +0600

    Add files via upload

---
+++ Conditional Statements/15/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the value of first Angle: ");
+            int angleA = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the value of second Angle: ");
+            int angleB = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the value of third Angle: ");
+            int angleC = Convert.ToInt32(Console.ReadLine());
+
+            int sum = angleA + angleB + angleC;
+
+            
+            Console.WriteLine(sum == 180
+                ?"The triangle is valid.": 
+                "The triangle is not valid.");
+            
+            Console.ReadKey();
+        }
+    }
+}
