4378a5e815993e597d174ba8fe943c5e558225cb
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-29 13:00:04 +0600

    Add files via upload

---
+++ For Loop/12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            for (int i = 0; i <5; i++)
+            {
+                for (int j =0 ; j <= i; j++)
+                {
+                    Console.Write(count+1);
+                    count++;
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
