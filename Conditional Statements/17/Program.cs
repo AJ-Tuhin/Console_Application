9e80dd70adc05d6a6bd978c5f10e3b5e3b2d17f8
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-29 10:42:06 +0600

    Add files via upload

---
+++ Conditional Statements/17/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the cost price: ");
+            int cost = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the selling price: ");
+            int sell = Convert.ToInt32(Console.ReadLine());
+
+            if (sell>cost)
+            {
+                Console.WriteLine("Your profit amount is: {0}",sell-cost);
+            }
+            else if (sell < cost)
+            {
+                Console.WriteLine("Your loss amount is: {0}", cost-sell);
+            }
+            else
+            {
+                Console.WriteLine("Your are in no profit no loss condition");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
