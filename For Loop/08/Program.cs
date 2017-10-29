2d07daf7f8f351d65f8e6d8dd779a24409d0ef4c
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-29 12:53:34 +0600

    Add files via upload

---
+++ For Loop/08/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input number of terms: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            int j = 1;
+            int count = 0; 
+            Console.Write("\nThe odd numbers are: ");
+            while (count < number)
+            {
+                if (j % 2 != 0)
+                {
+                    Console.Write(j + ",");
+                    sum += j;
+                    count++;
+                }
+                j++;
+            }
+            
+            Console.WriteLine("\nThe Sum of odd Natural Number upto 10 terms : {0}",sum);
+            Console.ReadKey();
+        }
+    }
+}
