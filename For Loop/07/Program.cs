6d098234065ba2f026bd784508688396ec77e057
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-29 12:52:48 +0600

    Add files via upload

---
+++ For Loop/07/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input upto the table number starting from 1: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= number; j++)
+                {
+                    Console.Write("{0}x{1} = {2}, ",j,i,i*j);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
