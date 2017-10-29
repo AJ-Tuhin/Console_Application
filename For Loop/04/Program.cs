042cd59faca661b68db5b3757d4634c76f46fb3d
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-29 12:49:39 +0600

    Add files via upload

---
+++ For Loop/04/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the 10 numbers:");
+            double result = 0;
+            for (int i = 1; i < 11; i++)
+            {
+                Console.Write("Number-{0}: ",i);
+                double number = Convert.ToInt32(Console.ReadLine());
+                result += number;
+            }
+            double average = result/10;
+            Console.WriteLine("The sum of 10 no is: {0}",result);
+            Console.WriteLine("The Average is : {0}", average);
+            Console.ReadKey();
+        }
+    }
+}
