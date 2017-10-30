41c2e33dd8a4d7eaa334b6dea74fc87079970545
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-30 11:27:24 +0600

    Add files via upload

---
+++ For Loop/15/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int result = 1;
+            for (int i = 1; i <=number ; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine("The Factorial of {0} is:{1}",number,result);
+            Console.ReadKey();
+        }
+    }
+}
