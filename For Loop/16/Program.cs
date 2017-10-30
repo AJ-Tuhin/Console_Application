335f2cc1c0eded766a8a0f262194b8498027a8fd
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-30 11:28:33 +0600

    Add files via upload

---
+++ For Loop/16/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input number of terms: ");
+            int number =Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            int sum = 0;
+            Console.Write("The even numbers are: ");
+            for (int i = 1; count<number; i++)
+            {
+                if (i%2==0)
+                {
+                    sum += i;
+                    Console.Write(i+" ");
+                    count++;
+                }
+            }
+            
+            Console.WriteLine("\nThe Sum of even Natural Number upto {0} terms : {1}",number,sum);
+            Console.ReadKey();
+        }
+    }
+}
