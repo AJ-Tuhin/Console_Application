f1d78f1c8353eaacc076c4dccf6b133b01a59bde
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-30 11:24:49 +0600

    Add files via upload

---
+++ For Loop/13/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of row: ");
+            int row = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            int spc = row - 1;
+            for (int i = 1; i <= row; i++)
+            {
+                for (int k = spc; k >= 1; k--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <=i; j++)
+                {
+                    count++;
+                    Console.Write(count+" ");
+                }
+                Console.WriteLine();
+                spc--;
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
