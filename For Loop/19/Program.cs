b8295d533999b052aff540aea7191254111779a7
Md. Al Jobaed <tuhincse001@gmail.com>
2017-10-30 16:46:58 +0600

    Add files via upload

---
+++ For Loop/19/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the number of terms: ");
+            double term = Convert.ToDouble(Console.ReadLine());
+
+            double sum = 0;
+            for (int i = 1; i <=term; i++)
+            {
+                double d = 1/ (double)i;
+                sum += d;
+            }
+            Console.WriteLine("Sum of Series upto {0} terms :{1}",term,sum);
+            Console.ReadKey();
+        }
+    }
+}
