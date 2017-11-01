848bebdfc256c836ca2ac42661977e11afbe72be
Md. Al Jobaed <tuhincse001@gmail.com>
2017-11-01 18:34:16 +0600

    Add files via upload

---
+++ Array/01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] element = new int[10];
+            for (int i = 0; i < element.Length; i++)
+            {
+                Console.Write("Element {0}: ",i);
+                element[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            foreach (var value in element)
+            {
+                Console.Write(value+" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
