809af8c7d959737429987fe6d4909aed102ca6a0
Md. Al Jobaed <tuhincse001@gmail.com>
2017-11-05 15:43:30 +0600

    Add files via upload

---
+++ For Loop/31/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31.app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of half of diamond: ");
+            int r = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= r; i++)
+            {
+                for (int j = 1; j <= r - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= 2*i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i =r-1; i >=1; i--)
+            {
+                for (int j = 1; j <= r - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+        
+      } 
+}
