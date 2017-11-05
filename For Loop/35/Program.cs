de6d9fdec3fdd9b6c48f3156059d1dc538a0291f
Md. Al Jobaed <tuhincse001@gmail.com>
2017-11-05 15:48:29 +0600

    Add files via upload

---
+++ For Loop/35/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _35.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of term: ");
+            int term = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0, a=0, temp=0;
+            for (int i = 0; i <term; i++)
+            {
+                a = temp-a;
+                sum += a;
+                temp = sum;
+                Console.Write(sum+" ");
+                if (temp==0)
+                {
+                    temp++;
+                }
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
