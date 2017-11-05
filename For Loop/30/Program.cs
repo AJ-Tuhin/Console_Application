e60cb56111cc6abd33e0b1c78f65b04916853bf0
Md. Al Jobaed <tuhincse001@gmail.com>
2017-11-05 15:42:30 +0600

    Add files via upload

---
+++ For Loop/30/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input starting number of range: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input ending number of range: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int reminder;
+            int sum;
+            for (int i = a; i <=b; i++)
+            {
+                sum = 0;
+                for (int j = i; j !=0; j=j/10)
+                {
+                    reminder = j%10;
+                    sum = sum + (reminder*reminder*reminder);
+                }
+                if (sum==i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
