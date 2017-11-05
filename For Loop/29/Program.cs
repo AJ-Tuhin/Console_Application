1df2dc1220c5cab0f12724c5f550c39a779029ee
Md. Al Jobaed <tuhincse001@gmail.com>
2017-11-05 15:41:04 +0600

    Add files via upload

---
+++ For Loop/29/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int reminder;
+            int sum =0;
+            Console.Write("Enter a number of 3 digit: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = num; i >0 ; i=i/10)
+            {
+                reminder = i%10;
+                sum = sum + reminder*reminder*reminder;
+            }
+            Console.WriteLine(sum == num ? "{0} is an Armstrong number." : "{0} is not an Armstrong number.", num);
+            Console.ReadKey();
+        }
+    }
+}
