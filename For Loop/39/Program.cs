f6574c1a270ec3183d845a55e8048847f4ee30c4
Md. Al Jobaed <tuhincse001@gmail.com>
2017-11-06 18:38:53 +0600

    Add files via upload

---
+++ For Loop/39/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _39.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.Write("Numbers between 100 and 200, divisible by 9: \n");
+            for (int i = 100; i <=200; i++)
+            {
+                if (i%9==0)
+                {
+                    Console.Write(i+" ");
+                    sum += i;
+                }
+            }
+            Console.WriteLine("\nThe sum:{0}",sum);
+            Console.ReadKey();
+        }
+    }
+}
