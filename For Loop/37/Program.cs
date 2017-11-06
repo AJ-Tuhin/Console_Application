a060f80038fe45d03032a258c4d56d3fc5709d39
Md. Al Jobaed <tuhincse001@gmail.com>
2017-11-06 18:36:40 +0600

    Add files via upload

---
+++ For Loop/37/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number you want to reverse: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("The number in reverse order is: ");
+            for (int i = a; i !=0 ;i= i / 10)
+            {
+                int reminder = i%10;
+                Console.Write(reminder);
+            }
+            Console.ReadKey();
+        }
+    }
+}
