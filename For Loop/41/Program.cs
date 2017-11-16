61148a2f8e4684eed304c9ec9182aad74c11d7a1
Md. Al Jobaed <tuhincse001@gmail.com>
2017-11-16 19:53:19 +0600

    Add files via upload

---
+++ For Loop/41/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _41.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number to convert: ");
+            int dNumber = Convert.ToInt32(Console.ReadLine());
+            string result = string.Empty;
+
+            for (int i = dNumber; i > 0; i = i / 2)
+            {
+                int reminder = i % 2;
+                result = reminder+result;
+                
+            }
+            Console.WriteLine(result);
+
+            //string binary = Convert.ToString(dNumber, 2);
+            //Console.WriteLine(binary);
+
+            Console.ReadKey();
+        }
+    }
+}
