dede9b831888144511b442b138bc051e03d7de05
Md. Al Jobaed <tuhincse001@gmail.com>
2017-11-16 21:25:12 +0600

    Add files via upload

---
+++ For Loop/43/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _43.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input 1st number for HCF: ");
+            int fNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input 2nd number for HCF: ");
+            int lNumber = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            int j = fNumber < lNumber ? fNumber : lNumber;
+
+            for (int i = 1; i <=j; i++)
+            {
+                if (fNumber%i==0 && lNumber%i==0)
+                {
+                    result = i;
+                }
+            }
+            Console.WriteLine("HCF of {0} and {1} is: {2} ",fNumber,lNumber,result);
+            Console.ReadKey();
+        }
+    }
+}
