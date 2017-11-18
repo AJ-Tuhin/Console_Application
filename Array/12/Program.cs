53e5b7da7753f6de4db104e681322346c9eb48f6
Md. Al Jobaed <tuhincse001@gmail.com>
2017-11-18 17:11:56 +0600

    Add files via upload

---
+++ Array/12/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the size of array: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr1 = new int[a];
+
+            Console.WriteLine("Input {0} elements in the array: ",a);
+            for (int i = 0; i <a; i++)
+            {
+                Console.Write("element - {0}: ",i);
+                arr1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = i+1; j < a; j++)
+                {
+                    if (arr1[i] < arr1[j])
+                    {
+                        int temp = arr1[i];
+                        arr1[i] = arr1[j];
+                        arr1[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Elements of the array in sorted descending order: ");
+            foreach (int i in arr1)
+            {
+                Console.Write(i+" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
