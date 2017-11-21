ba9c363f75d823f8ea63b3a6078aab1fa1d0eabe
Md. Al Jobaed <tuhincse001@gmail.com>
2017-11-21 21:48:27 +0600

    Add files via upload

---
+++ For Loop/51/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _51.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input an octal number (using digit 0 - 7): ");
+            uint octal = Convert.ToUInt32(Console.ReadLine());
+
+            uint a = 0;
+            uint sum = 0;
+
+            for (uint i = octal; i >0; i=i/10)
+            {
+                uint reminder = i%10;
+                double b = reminder * Math.Pow(8, a);
+                sum += (uint)b;
+                a++;
+            }
+            Console.WriteLine("The Octal Number: {0}\nThe equivalent Decimal Number: {1}",octal,sum);
+            Console.ReadKey();
+        }
+    }
+}
