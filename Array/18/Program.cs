e7e9613ba54f672ee3d4ee1c70cf3859e0523481
Md. Al Jobaed <tuhincse001@gmail.com>
2018-02-04 09:36:22 +0600

    Add files via upload

---
+++ Array/18/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[3,3];
+
+            Console.WriteLine("Input elements in the matrix : ");
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write("element - [{0},{1}] : ", i,j);
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("\nThe matrix is : \n");
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(arr[i,j]+"  ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
